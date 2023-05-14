3409df9d3e5e706bb7083ca1b222a879a2a1b632
vannguyenv12 <d.bed16@gptgdt.net>
2023-05-14 19:05:30 +0700

    Add input anywhere

--- js/map.js
+++ js/map.js
@@ -1,18 +1,77 @@
-function renderMap() {
-  mapboxgl.accessToken =
-    "pk.eyJ1IjoidmFubmd1eWVudjEyIiwiYSI6ImNsaG5hcW5oZzBlb2Mzam5ndTFodTJ2Z2EifQ.Q8I2IbBuQRGGrOLF-WIUlg"; // Thay YOUR_ACCESS_TOKEN bằng mã truy cập của bạn
+async function getCoordinatesFromAddress(addressInput) {
+  try {
+    var accessToken =
+      "pk.eyJ1IjoidmFubmd1eWVudjEyIiwiYSI6ImNsaG5hcW5oZzBlb2Mzam5ndTFodTJ2Z2EifQ.Q8I2IbBuQRGGrOLF-WIUlg";
 
-  const map = new mapboxgl.Map({
-    container: "map",
-    style: "mapbox://styles/mapbox/streets-v11",
-    center: [108.2772, 14.0583],
-    zoom: 5,
+    var address = addressInput;
+
+    var geocodingUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
+      address
+    )}.json?access_token=${accessToken}`;
+
+    var response = await fetch(geocodingUrl);
+    var data = await response.json();
+
+    var coordinates = data.features[0].geometry.coordinates;
+    var longitude = coordinates[0];
+    var latitude = coordinates[1];
+
+    console.log("Kinh độ:", longitude);
+    console.log("Vĩ độ:", latitude);
+
+    mapboxgl.accessToken =
+      "pk.eyJ1IjoidmFubmd1eWVudjEyIiwiYSI6ImNsaG5hcW5oZzBlb2Mzam5ndTFodTJ2Z2EifQ.Q8I2IbBuQRGGrOLF-WIUlg"; // Thay YOUR_ACCESS_TOKEN bằng mã truy cập của bạn
+
+    const map = new mapboxgl.Map({
+      container: "map",
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [longitude, latitude],
+      zoom: 10,
+    });
+
+    const marker = new mapboxgl.Marker()
+      .setLngLat([longitude, latitude])
+      .setPopup(new mapboxgl.Popup().setHTML("<h5>I'm here!</h5>"))
+      .addTo(map);
+  } catch (error) {
+    console.log("Lỗi:", error);
+  }
+}
+getCoordinatesFromAddress(
+  "140 Lê Trọng Tấn, Phường Tây Thạnh, Quận Tân Phú, TP.HCM"
+);
+
+// async function renderMap() {
+//   const { longitude, latitude } = await getCoordinatesFromAddress();
+//   console.log(longitude, latitude);
+//   mapboxgl.accessToken =
+//     "pk.eyJ1IjoidmFubmd1eWVudjEyIiwiYSI6ImNsaG5hcW5oZzBlb2Mzam5ndTFodTJ2Z2EifQ.Q8I2IbBuQRGGrOLF-WIUlg"; // Thay YOUR_ACCESS_TOKEN bằng mã truy cập của bạn
+
+//   const map = new mapboxgl.Map({
+//     container: "map",
+//     style: "mapbox://styles/mapbox/streets-v11",
+//     center: [longitude, latitude],
+//     zoom: 12,
+//   });
+
+//   const marker = new mapboxgl.Marker()
+//     .setLngLat([longitude, latitude])
+//     .setPopup(new mapboxgl.Popup().setHTML("<h5>I'm here!</h5>"))
+//     .addTo(map);
+// }
+
+// renderMap();
+
+function handleSubmit() {
+  const formAdressEl = document.querySelector(".form-address");
+  const inputAddressEl = formAdressEl.querySelector(".address-input");
+
+  formAdressEl.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    await getCoordinatesFromAddress(inputAddressEl.value);
+    inputAddressEl.value = "";
   });
-
-  const marker = new mapboxgl.Marker()
-    .setLngLat([106.6297, 10.8231])
-    .setPopup(new mapboxgl.Popup().setHTML("<h5>I'm here!</h5>"))
-    .addTo(map);
 }
 
-renderMap();
+handleSubmit();
